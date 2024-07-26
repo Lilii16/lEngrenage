b21ed74cc807db43d8dce953ac78c5fb9e65e9ca
Ligia Aranda <arandaligia8@gmail.com>
2024-07-26 17:00:22 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+include_once './utilities/composantsG/header.php';
+include_once './utilities/navigation/navGenerale.php';
+?>
+
+<div id="accueil" class="container-fluid about fit-text d-flex flex-column justify-content-end text-start">
+    <h2 class="p-4 mx-2 fs-2">ACCUEIL</h2>
+</div>
+<div class="image-overlay-container position-relative">
+    <div class="bg-black">
+        <img src="assets/images/fuseeCoupee.png" class="img-fluid w-75" alt="Image d'arrière-plan">
+    </div>
+    <div class="yellow-rec position-absolute w-100 h-100"></div>
+    <div class="yellow-rec-vertical">
+    <div class="text-overlay mt-2 p-4 fs-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+         sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="text-overlay p-4 fs-4">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod 
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in 
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+         sunt in culpa qui officia deserunt mollit anim id est laborum.
+    </div>
+    <div class="text-overlay text-end p-4 fs-4">
+        Lorem ipsum dolor sit amet.
+    </div>
+</div>
---
+++ utilities/navigation/navGenerale.php
@@ -0,0 +1,56 @@
+<div class="row">
+    <div class="col-md image-container">
+      <img src="/LENGRENAGE/assets/images/banniere.png" class="img-fluid object-fit-cover" alt="Image d'arrière-plan">
+    </div>
+</div>
+<div class="container-fluid fit-container">
+    <div class="fit-text">
+    <h1>L'ENGRENAGE</h1>
+    </div>
+</div>
+<nav class="navbar navbar-expand-lg">
+    <div class="menu container-fluid">
+        <!-- Bouton pour basculer la navigation sur mobile -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- Contenu de la barre de navigation -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav d-flex w-100 justify-content-center space-around">
+                <div class="row w-100 text-center">
+                    <!-- Chaque élément de la barre de navigation est placé dans une colonne pour l'alignement -->
+                    <div class="col-md">
+                        <!-- Lien vers la page d'accueil -->
+                        <li class="nav-item flex-grow-1">
+                            <a class="nav-link active fw-2" aria-current="page" href="index.php#accueil">ACCUEIL</a>
+                        </li>
+                    </div>
+                    <div class="col-md">
+                        <!-- Lien vers la page des compétences -->
+                        <li class="nav-item flex-grow-1">
+                            <a class="nav-link fw-2" href="../dates.php">DATES</a>
+                        </li>
+                    </div>
+                    <div class="col-md">
+                        <!-- Lien vers la page des honoraires -->
+                        <li class="nav-item flex-grow-1">
+                            <a class="nav-link" href="../nous.php">ET NOUS ?</a>
+                        </li>
+                    </div>
+                    <div class="col-md">
+                        <!-- Lien vers la page de conseils -->
+                        <li class="nav-item flex-grow-1">
+                            <a class="nav-link" aria-disabled="true" href="../creations.php">CREATIONS</a>
+                        </li>
+                    </div>
+                    <div class="col-md">
+                        <!-- Lien vers la page de contact -->
+                        <li class="nav-item flex-grow-1">
+                            <a class="nav-link" aria-disabled="true" href="../contact.php">CONTACT</a>
+                        </li>
+                    </div>
+                </div>
+            </ul>
+        </div>
+    </div>
+</nav>
